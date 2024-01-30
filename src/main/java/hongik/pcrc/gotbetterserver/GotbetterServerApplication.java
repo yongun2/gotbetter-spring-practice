a40bd4d40e3b7a78b4fff6d457c9c457cd55a0a7
yongun2 <djkye0001@naver.com>
2024-01-30 21:10:57 +0900

    project init

---
+++ src/main/java/hongik/pcrc/gotbetterserver/GotbetterServerApplication.java
@@ -0,0 +1,13 @@
+package hongik.pcrc.gotbetterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotbetterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GotbetterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hongik/pcrc/gotbetterserver/GotbetterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.pcrc.gotbetterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotbetterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

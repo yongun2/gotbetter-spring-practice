33b0a18a6ff51f86ca60f5d2326a270a06651dc3
yongun2 <djkye0001@naver.com>
2024-01-31 19:55:53 +0900

    [FEAT] user repository with CrudRepository

--- src/main/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/entity/package-info.java
+++
@@ -1,1 +0,0 @@
-package hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.entity;
---
+++ src/main/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.repository;
+
+import hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+    Optional<UserEntity> findUserEntityByUserId(String userId);
+}

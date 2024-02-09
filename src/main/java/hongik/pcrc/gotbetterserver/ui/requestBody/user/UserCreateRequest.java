e34dbed5ebaa3bdbbefd0c3d9df25b4e0a580fe1
yongun2 <djkye0001@naver.com>
2024-02-09 22:38:27 +0900

    [UPDATE] 빈 validation 제거

--- src/main/java/hongik/pcrc/gotbetterserver/ui/requestBody/user/UserCreateRequest.java
+++ src/main/java/hongik/pcrc/gotbetterserver/ui/requestBody/user/UserCreateRequest.java
@@ -5,19 +5,15 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 @ToString
 @NoArgsConstructor
 public class UserCreateRequest {
     @NotNull
-    @Length(max = 12)
     private String userId;
     @NotBlank
-    @Length(max = 20)
     private String password;
     @NotBlank
-    @Length(max = 10)
     private String nickname;
 }

f661d943cf279d4f1eb1d773041aa448a8434944
yongun2 <djkye0001@naver.com>
2024-02-09 22:35:33 +0900

    [FEAT] 회원가입, 중복확인 컨트롤러

--- src/main/java/hongik/pcrc/gotbetterserver/ui/controller/UserController.java
+++ src/main/java/hongik/pcrc/gotbetterserver/ui/controller/UserController.java
@@ -7,16 +7,22 @@ import hongik.pcrc.gotbetterserver.exception.MessageType;
 import hongik.pcrc.gotbetterserver.ui.requestBody.user.UserCreateRequest;
 import hongik.pcrc.gotbetterserver.ui.view.ApiResponseView;
 import hongik.pcrc.gotbetterserver.ui.view.user.UserView;
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
@@ -24,10 +30,33 @@ public class UserController {
     private final PasswordEncoder passwordEncoder;
 
 
-    @PostMapping("/users")
+    @PostMapping("")
     public ResponseEntity<ApiResponseView<UserView>> signup(@RequestBody @Validated UserCreateRequest createRequest) {
 
-        // password encode
+        // check id pattern
+        String userIdPattern = "^[a-z0-9_-]{5,20}$";
+        Matcher userIdMatcher = Pattern.compile(userIdPattern)
+                .matcher(createRequest.getUserId());
+
+        if (!userIdMatcher.matches()) {
+            throw new GotbetterException(MessageType.BAD_USER_ID_PATTERN);
+        }
+
+        // check password pattern
+        String passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()-_=+\\\\|{};:'\",<.>/?])[A-Za-z\\d!@#$%^&*()-_=+\\\\|{};:'\",<.>/?]{8,16}$";
+        Matcher passwordMatcher = Pattern.compile(passwordPattern)
+                .matcher(createRequest.getPassword());
+
+        if (!passwordMatcher.matches()) {
+            throw new GotbetterException(MessageType.BAD_PASSWORD_PATTERN);
+        }
+
+        // check nickname pattern
+        if (createRequest.getNickname().length() < 4 || createRequest.getNickname().length() > 12) {
+            throw new GotbetterException(MessageType.BAD_NICKNAME_PATTERN);
+        }
+
+        // password encode before store
         String encodedPassword = passwordEncoder.encode(createRequest.getPassword());
 
         User createdUser = userService.createUser(
@@ -44,17 +73,54 @@ public class UserController {
 
     }
 
-    @GetMapping("/users")
-    public ResponseEntity<ApiResponseView<UserView>> checkDuplicate(@RequestParam String userId) {
-        boolean isDuplicate = userService.checkUserIdDuplicate(userId);
+    @GetMapping("/duplicate")
+    public ResponseEntity<ApiResponseView<UserView>> checkDuplicate(@RequestParam @Nullable String userId, @RequestParam @Nullable String nickname) {
 
-        if (isDuplicate) {
-            throw new GotbetterException(MessageType.DUPLICATED_ID);
+        if (userId != null && nickname != null) {
+            throw new GotbetterException(MessageType.BAD_REQUEST);
         }
 
-        User passUserId = User.builder().userId(userId).build();
+        boolean isDuplicate;
+        User result = null;
+        // check is userId duplicate
+        if (userId != null) {
+
+            if(userId.isBlank()) {
+                throw new GotbetterException(MessageType.BAD_USER_ID_PATTERN);
+            }
+
+            isDuplicate = userService.checkUserIdDuplicate(userId);
+            if (isDuplicate) {
+                throw new GotbetterException(MessageType.DUPLICATED_USER_ID);
+            }
+
+            result = User.builder()
+                    .userId(userId)
+                    .build();
+        }
+
+        // check is nickname duplicate
+        if (nickname != null) {
+
+            if(nickname.isBlank()) {
+                throw new GotbetterException(MessageType.BAD_NICKNAME_PATTERN);
+            }
+
+            isDuplicate = userService.checkUserNicknameDuplicate(nickname);
+            if (isDuplicate) {
+                throw new GotbetterException(MessageType.DUPLICATE_NICKNAME);
+            }
+            result = User.builder()
+                    .nickname(nickname)
+                    .build();
+        }
+
+        if (result == null) {
+            throw new GotbetterException(MessageType.INTERNAL_SERVER_ERROR);
+        }
+
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(new ApiResponseView<>(new UserView(passUserId)));
+                .body(new ApiResponseView<>(new UserView(result)));
     }
 }

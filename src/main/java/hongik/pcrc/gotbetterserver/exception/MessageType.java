87a79074fbb232de12a86c3efb3cd31c039897c5
yongun2 <djkye0001@naver.com>
2024-02-09 22:34:34 +0900

    [FEAT] 회원가입 과정 오류 메세지 추가

--- src/main/java/hongik/pcrc/gotbetterserver/exception/MessageType.java
+++ src/main/java/hongik/pcrc/gotbetterserver/exception/MessageType.java
@@ -5,10 +5,19 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum MessageType {
-    BAD_REQUEST ("Check API request URL protocol, parameter, etc. for errors", HttpStatus.BAD_REQUEST),
-    NOT_FOUND ("No data was found for the server. Please refer  to parameter description.", HttpStatus.NOT_FOUND),
-    INTERNAL_SERVER_ERROR ("An error occurred inside the server.", HttpStatus.INTERNAL_SERVER_ERROR),
-    DUPLICATED_ID("Duplicated user ID", HttpStatus.CONFLICT);
+
+    BAD_REQUEST("Check API request URL protocol, parameter, etc. for errors", HttpStatus.BAD_REQUEST),
+
+    // User signup 400
+    BAD_USER_ID_PATTERN("UserID must be 5-20 characters, lowercase letters, numbers, and special symbols (_), (-) only.", HttpStatus.BAD_REQUEST),
+    BAD_PASSWORD_PATTERN("Password must be 8 to 16 characters of upper and lowercase letters, numbers, and special characters.", HttpStatus.BAD_REQUEST),
+    BAD_NICKNAME_PATTERN("Nickname must be 4-12 characters", HttpStatus.BAD_REQUEST),
+    DUPLICATED_USER_ID("Duplicated userId", HttpStatus.CONFLICT),
+    DUPLICATE_NICKNAME("Duplicated Nickname", HttpStatus.CONFLICT),
+
+    NOT_FOUND("No data was found for the server. Please refer  to parameter description.", HttpStatus.NOT_FOUND),
+    INTERNAL_SERVER_ERROR("An error occurred inside the server.", HttpStatus.INTERNAL_SERVER_ERROR);
+
 
     private final String message;
     private final HttpStatus status;

dfbb17984ef0f5e8a677c62bcd50778f00058dda
yongun2 <djkye0001@naver.com>
2024-02-09 22:37:53 +0900

    [FEAT] 닉네임을 사용한 DB 조회 추가

--- src/main/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/repository/UserRepository.java
+++ src/main/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<UserEntity, Integer> {
     Optional<UserEntity> findUserEntityByUserId(String userId);
+
+    Optional<UserEntity> findUserEntityByNickname(String userId);
 }

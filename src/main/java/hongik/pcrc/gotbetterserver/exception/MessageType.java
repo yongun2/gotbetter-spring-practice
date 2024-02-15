21f8ca8c95c5ca92ea5114e97d067687d18f1062
yongun2 <djkye0001@naver.com>
2024-02-15 18:28:42 +0900

    [DOC] jwt 토큰 예외 메세지

--- src/main/java/hongik/pcrc/gotbetterserver/exception/MessageType.java
+++ src/main/java/hongik/pcrc/gotbetterserver/exception/MessageType.java
@@ -8,12 +8,18 @@ public enum MessageType {
 
     BAD_REQUEST("Check API request URL protocol, parameter, etc. for errors", HttpStatus.BAD_REQUEST),
 
-    // User signup 400
+    // User Error
     BAD_USER_ID_PATTERN("UserID must be 5-20 characters, lowercase letters, numbers, and special symbols (_), (-) only.", HttpStatus.BAD_REQUEST),
     BAD_PASSWORD_PATTERN("Password must be 8 to 16 characters of upper and lowercase letters, numbers, and special characters.", HttpStatus.BAD_REQUEST),
     BAD_NICKNAME_PATTERN("Nickname must be 4-12 characters", HttpStatus.BAD_REQUEST),
     DUPLICATED_USER_ID("Duplicated userId", HttpStatus.CONFLICT),
     DUPLICATE_NICKNAME("Duplicated Nickname", HttpStatus.CONFLICT),
+    USER_NOT_FOUND("No corresponding user was found.", HttpStatus.NOT_FOUND),
+
+    // jwt
+    INVALID_TOKEN("Received an invalid token.", HttpStatus.UNAUTHORIZED),
+    TOKEN_EXPIRED("The token has expired.", HttpStatus.BAD_REQUEST),
+
 
     NOT_FOUND("No data was found for the server. Please refer  to parameter description.", HttpStatus.NOT_FOUND),
     INTERNAL_SERVER_ERROR("An error occurred inside the server.", HttpStatus.INTERNAL_SERVER_ERROR);

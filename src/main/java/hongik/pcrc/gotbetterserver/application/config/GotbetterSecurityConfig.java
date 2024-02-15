8be4418e7b436162ee47b771fa07da8282a4d85f
yongun2 <djkye0001@naver.com>
2024-02-15 18:35:24 +0900

    [FEAT] jwt 토큰 검증 필터

--- src/main/java/hongik/pcrc/gotbetterserver/application/config/GotbetterSecurityConfig.java
+++ src/main/java/hongik/pcrc/gotbetterserver/application/config/GotbetterSecurityConfig.java
@@ -1,23 +1,71 @@
 package hongik.pcrc.gotbetterserver.application.config;
 
+import hongik.pcrc.gotbetterserver.application.filter.JWTTokenValidatorFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
+@RequiredArgsConstructor
 public class GotbetterSecurityConfig {
+
+    private final JWTTokenValidatorFilter jwtTokenValidatorFilter;
+
+    /**
+     * cors configuration
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PATCH", "PUT"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        return http.formLogin(AbstractHttpConfigurer::disable)
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .build();
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+
+        http
+                .authorizeHttpRequests(requests -> requests
+                        // 회원가입, 로그인 관련
+                        .requestMatchers(
+                                "/api/v1/users/register",
+                                "/api/v1/users/duplicate",
+                                "/api/v1/users/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users/test").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // 커스텀 필터 추가
+                .addFilterBefore(jwtTokenValidatorFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {

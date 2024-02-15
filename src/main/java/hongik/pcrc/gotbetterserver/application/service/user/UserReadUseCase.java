1d66423a38616e9886db97a5de46d2aa002727be
yongun2 <djkye0001@naver.com>
2024-02-15 18:34:07 +0900

    [FEAT] 로그인, 로그아웃 기능 추가

--- src/main/java/hongik/pcrc/gotbetterserver/application/service/user/UserReadUseCase.java
+++ src/main/java/hongik/pcrc/gotbetterserver/application/service/user/UserReadUseCase.java
@@ -1,8 +1,23 @@
 package hongik.pcrc.gotbetterserver.application.service.user;
 
+import hongik.pcrc.gotbetterserver.application.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
 public interface UserReadUseCase {
+
+    User findUserByUsername(String userId);
 
     boolean checkUserIdDuplicate(String userId);
 
     boolean checkUserNicknameDuplicate(String nickname);
+
+    @Getter
+    @Builder
+    @ToString
+    class LoginRequest {
+        private final String username;
+        private final String password;
+    }
 }

2a87e9f5662014622d5fffd0a64a483d435e920d
yongun2 <djkye0001@naver.com>
2024-02-23 16:07:02 +0900

    [TEST] 변경사항 테스트 반영

--- src/test/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/repository/UserRepositoryTest.java
+++ src/test/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/repository/UserRepositoryTest.java
@@ -1,22 +1,21 @@
 package hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.repository;
 
 import hongik.pcrc.gotbetterserver.application.domain.User;
+import hongik.pcrc.gotbetterserver.application.domain.auth.JWTAuthenticationToken;
+import hongik.pcrc.gotbetterserver.application.domain.auth.JWTToken;
+import hongik.pcrc.gotbetterserver.application.domain.auth.RefreshToken;
+import hongik.pcrc.gotbetterserver.application.service.auth.JWTTokenProvider;
 import hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.entity.UserEntity;
-import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.After;
-import org.assertj.core.api.Assertions;
-import org.hibernate.resource.transaction.spi.TransactionStatus;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,43 +28,47 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+
+    @AfterEach
+    void deleteDummy() {
+        userRepository.deleteAll();
+    }
+
+
+
     /**
-     * UNIQUE Constraint 테스트 위해 @Transactional 제외
+     * UNIQUE Constraint, Max Length 테스트 위해 @Transactional 제외
      */
     @Test
     @DisplayName("유저 생성 테스트")
     void createUser() {
         // given
-        User testUserB = User.builder()
+        User saveSuccess = User.builder()
                 .username("asd1234")
                 .password("qwer1234!")
-                .nickname("testUserB")
+                .nickname("testUserA")
                 .build();
         User testUserDuplicateUsername = User.builder()
                 .username("asd1234")
                 .password("qwer1234!")
-                .nickname("testUserC")
+                .nickname("testUserB")
                 .build();
         User testUserDuplicateNickname = User.builder()
                 .username("asd1235")
                 .password("qwer1234!")
-                .nickname("testUserB")
+                .nickname("testUserA")
                 .build();
         // when
-        UserEntity testUserA_result = userRepository.saveAndFlush(new UserEntity(testUserB));
+        userRepository.saveAndFlush(new UserEntity(saveSuccess));
         // then
-
-        // success
-        assertThat(testUserA_result.getUsername()).isEqualTo(testUserB.getUsername());
-        assertThat(testUserA_result.getRefreshTokenEntity()).isNull();
 
         // Error test
         assertThatThrownBy(() -> userRepository.save(new UserEntity(testUserDuplicateUsername)))
                 .isInstanceOf(DataIntegrityViolationException.class);
         assertThatThrownBy(() -> userRepository.save(new UserEntity(testUserDuplicateNickname)))
                 .isInstanceOf(DataIntegrityViolationException.class);
-
-        userRepository.delete(testUserA_result);
     }
 
     @Test
@@ -73,44 +76,39 @@ class UserRepositoryTest {
     @DisplayName("유저 조회 테스트")
     void findUserBy_() {
         // given
-        User testUserB = User.builder()
+        User testUserA = User.builder()
                 .username("asd1234")
                 .password("qwer1234!")
-                .nickname("testUserB")
+                .nickname("testUserA")
                 .build();
-        UserEntity testUserB_result = userRepository.save(new UserEntity(testUserB));
+
+        UserEntity result = userRepository.save(new UserEntity(testUserA));
 
         // when
-        Optional<UserEntity> resultByUsername = userRepository.findUserEntityByUsername(testUserB_result.getUsername());
-        Optional<UserEntity> resultByNickname = userRepository.findUserEntityByNickname(testUserB_result.getNickname());
+        Optional<UserEntity> resultByUsername = userRepository.findUserEntityByUsername(result.getUsername());
+        Optional<UserEntity> resultByNickname = userRepository.findUserEntityByNickname(result.getNickname());
         Optional<UserEntity> failed_nickname = userRepository.findUserEntityByNickname("");
         Optional<UserEntity> failed_username = userRepository.findUserEntityByUsername("");
+
         // then
 
-        resultByUsername.ifPresent(
-                (userEntity) -> {
-                    User user = userEntity.toUser();
-                    assertThat(user.getId()).isEqualTo(testUserB_result.getId());
-                    assertThat(user.getUsername()).isEqualTo(testUserB_result.getUsername());
-                    assertThat(user.getRefreshToken()).isNull();
-                }
-        );
+        assertThat(resultByUsername.isPresent()).isTrue();
+        assertThat(resultByUsername.get().getId()).isEqualTo(result.getId());
+        assertThat(resultByUsername.get().getUsername()).isEqualTo(result.getUsername());
+        assertThat(resultByUsername.get().getRefreshTokenEntity()).isNull();
 
-        resultByNickname.ifPresent(
-                (userEntity) -> {
-                    User user = userEntity.toUser();
-                    assertThat(user.getId()).isEqualTo(testUserB_result.getId());
-                    assertThat(user.getNickname()).isEqualTo(testUserB_result.getNickname());
-                    assertThat(user.getRefreshToken()).isNull();
-                }
-        );
+        assertThat(resultByNickname.isPresent()).isTrue();
+        assertThat(resultByNickname.get().getId()).isEqualTo(result.getId());
+        assertThat(resultByNickname.get().getUsername()).isEqualTo(result.getUsername());
+        assertThat(resultByNickname.get().getRefreshTokenEntity()).isNull();
 
         assertThat(failed_nickname.isEmpty()).isTrue();
         assertThat(failed_username.isEmpty()).isTrue();
+
+
     }
 
     @Test
-    @Transactional
     @DisplayName("유저 수정 테스트")
     void updateUser() {
         // given
@@ -130,12 +128,125 @@ class UserRepositoryTest {
                 .email("hello@gmail.com")
                 .build();
 
+        User fail_updateUser_long_username = User.builder()
+                .id(defaultUserEntity.getId())
+                .username("asd1235asd1235asd1235asd1235")
+                .nickname("testUserC")
+                .email("hello@gmail.com")
+                .build();
+
+        User fail_updateUser_long_nickname = User.builder()
+                .id(defaultUserEntity.getId())
+                .username("asd1235asd1235asd1235asd1235")
+                .nickname("testUserCtestUserCtestUserCtestUserCtestUserCtestUserCtestUserCtestUserC")
+                .email("hello@gmail.com")
+                .build();
+
+
         UserEntity updateResult = userRepository.save(new UserEntity(updateUser));
         //then
-        log.info(defaultUserSaveResult.toString());
-        log.info(updateResult.toUser().toString());
+
         assertThat(updateResult.getUsername()).isNotEqualTo(defaultUserSaveResult.getUsername());
         assertThat(updateResult.getEmail()).isNotEqualTo(defaultUserSaveResult.getEmail());
         assertThat(updateResult.toUser().toString()).isNotSameAs(defaultUserSaveResult.toString());
+
+        assertThatThrownBy(() -> userRepository.save(new UserEntity(fail_updateUser_long_username)))
+                .isInstanceOf(DataIntegrityViolationException.class);
+        assertThatThrownBy(() -> userRepository.save(new UserEntity(fail_updateUser_long_nickname)))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+
+    @Test
+    @DisplayName("리프레시 토큰 저장")
+    void refreshToken() {
+        // given
+        User defaultUser = User.builder()
+                .username("asd1234")
+                .password("qwer1234!")
+                .nickname("testUserB")
+                .build();
+
+        userRepository.save(new UserEntity(defaultUser)); // 회원가입
+
+        // when
+        Optional<UserEntity> findResult = userRepository.findUserEntityByNickname(defaultUser.getNickname());
+        assertThat(findResult.isPresent()).isTrue();
+
+        UserEntity loginUserEntity = findResult.get();
+
+        JWTAuthenticationToken authenticationToken = new JWTAuthenticationToken(defaultUser.getUsername(), null);
+        JWTToken jwtToken = jwtTokenProvider.generateToken(authenticationToken);
+        RefreshToken refreshToken = RefreshToken.builder()
+                .token(jwtToken.getRefreshToken())
+                .build();
+
+        User storeRefreshToken = User.builder()
+                .id(loginUserEntity.getId())
+                .username(loginUserEntity.getUsername())
+                .password(loginUserEntity.getPassword())
+                .nickname(loginUserEntity.getNickname())
+                .email(loginUserEntity.getEmail())
+                .refreshToken(refreshToken)
+                .build();
+
+        UserEntity save = userRepository.save(new UserEntity(storeRefreshToken));
+        // then
+        assertThat(save.toUser().getRefreshToken().getToken()).isEqualTo(jwtToken.getRefreshToken());
+    }
+
+    @Test
+    @DisplayName("리프레시 토큰 삭제")
+    void deleteRefreshToken() {
+        // given
+        User defaultUser = User.builder()
+                .username("asd1234")
+                .password("qwer1234!")
+                .nickname("testUserB")
+                .build();
+
+        userRepository.save(new UserEntity(defaultUser)); // 회원가입
+
+        // when
+        Optional<UserEntity> findResult = userRepository.findUserEntityByNickname(defaultUser.getNickname());
+        assertThat(findResult.isPresent()).isTrue();
+
+        UserEntity loginUserEntity = findResult.get();
+
+        JWTAuthenticationToken authenticationToken = new JWTAuthenticationToken(defaultUser.getUsername(), null);
+        JWTToken jwtToken = jwtTokenProvider.generateToken(authenticationToken);
+        RefreshToken refreshToken = RefreshToken.builder()
+                .token(jwtToken.getRefreshToken())
+                .build();
+
+        User storeRefreshToken = User.builder()
+                .id(loginUserEntity.getId())
+                .username(loginUserEntity.getUsername())
+                .password(loginUserEntity.getPassword())
+                .nickname(loginUserEntity.getNickname())
+                .email(loginUserEntity.getEmail())
+                .refreshToken(refreshToken)
+                .build();
+
+        UserEntity save = userRepository.save(new UserEntity(storeRefreshToken));
+
+        User deleteRefreshToken = User.builder()
+                .id(save.getId())
+                .username(save.getUsername())
+                .password(save.getPassword())
+                .nickname(save.getNickname())
+                .email(save.getEmail())
+                .refreshToken(null)
+                .build();
+
+        UserEntity deleteResult = userRepository.save(new UserEntity(deleteRefreshToken));
+        // then
+        assertThat(deleteResult.getRefreshTokenEntity()).isNull();
+    }
+
+    @Test
+    @DisplayName("리프레시 토큰 갱신")
+    void updateRefreshToken() {
+
     }
 }

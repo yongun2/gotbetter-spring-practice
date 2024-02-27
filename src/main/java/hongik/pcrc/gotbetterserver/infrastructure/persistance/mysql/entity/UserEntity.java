44168f87036a38fcad54da362c82c0826bb82d55
yongun2 <djkye0001@naver.com>
2024-02-27 15:25:28 +0900

    [FEAT] UserEntity와 ParticipantEntity 양방향 관계 매핑

--- src/main/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/entity/UserEntity.java
+++ src/main/java/hongik/pcrc/gotbetterserver/infrastructure/persistance/mysql/entity/UserEntity.java
@@ -1,9 +1,14 @@
 package hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.entity;
 
 import hongik.pcrc.gotbetterserver.application.domain.User;
+import hongik.pcrc.gotbetterserver.application.domain.studyroom.Participant;
+import hongik.pcrc.gotbetterserver.infrastructure.persistance.mysql.entity.studyroom.ParticipantEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -27,6 +32,9 @@ public class UserEntity {
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "refresh_token_id")
     private RefreshTokenEntity refreshTokenEntity;
+
+    @OneToMany(mappedBy = "userEntity", cascade = CascadeType.REMOVE)
+    private final List<ParticipantEntity> participantEntities = new ArrayList<>();
 
     public User toUser() {
         User.UserBuilder builder = User.builder()

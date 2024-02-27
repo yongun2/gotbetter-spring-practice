5558bd37f2ae782f0a5c4ea7439cf2b4179923bb
yongun2 <djkye0001@naver.com>
2024-02-27 15:29:59 +0900

    [FEAT] 참여코드 생성 유틸리티 클래스

---
+++ src/main/java/hongik/pcrc/gotbetterserver/application/utils/RandomStringGenerator.java
@@ -0,0 +1,19 @@
+package hongik.pcrc.gotbetterserver.application.utils;
+
+import java.security.SecureRandom;
+
+public class RandomStringGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom random = new SecureRandom();
+
+    private static final int STRING_LENGTH = 12;
+
+    public static String generateRandomString() {
+        StringBuilder sb = new StringBuilder(STRING_LENGTH);
+        for (int i = 0; i < STRING_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+}
